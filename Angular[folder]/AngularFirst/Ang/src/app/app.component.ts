0cc45b2067ca7336e027466ebda6bdbf6140e5aa
vitalika1234 <vitaliihoncharuk@gmail.com>
2019-05-31 14:16:37 +0300

    wow effect

--- Angular[folder]/AngularFirst/Ang/src/app/app.component.ts
+++ Angular[folder]/AngularFirst/Ang/src/app/app.component.ts
@@ -9,7 +9,7 @@ import {CookieService} from 'angular2-cookie/core';
 export class AppComponent {
   constructor(protected _cookieService: CookieService) {}
   isLogin = this.setCookie('isLogin', 'true');
-  names: User[] = [Vitalik, Tanya, Vika, Alex];
+  names: User[] = [Vitalik, Tanya, Vika, Alex, Mike, John, Dasha];
   changeCookie(): void {
     this.getObj('isLogin') ? this.setCookie('isLogin', 'false') : this.setCookie('isLogin', 'true');
   }
@@ -28,29 +28,56 @@ export interface User {
   name: string;
   password: string;
   mail: string;
+  photo: string;
 }
 
 const Vitalik: User = {
   login: 'vitalika123',
   name : 'Vitalii',
   password: 'Vitali4ok',
-  mail: 'vitalii@gmail.com'
+  mail: 'vitalii@gmail.com',
+  photo: '../assets/Photos/1.png'
 };
 const Tanya: User = {
   login: 'tanka999',
   name : 'Tanya',
   password: 'tanysha123',
-  mail: 'tani4ka@gmail.com'
+  mail: 'tani4ka@gmail.com',
+  photo: '../assets/Photos/2.jpg'
 };
 const Vika: User = {
   login: 'vikusa123',
   name : 'Viktoria',
   password: 'qwerty123',
-  mail: 'viktoriaPushka@gmail.com'
+  mail: 'viktoriaPushka@gmail.com',
+  photo: '../assets/Photos/3.jpg'
 };
 const Alex: User = {
   login: 'CoolBoy228',
   name : 'Alex',
   password: 'tryToGuessXD',
-  mail: 'alex1949@gmail.com'
+  mail: 'alex1949@gmail.com',
+  photo: '../assets/Photos/4.jpg'
+};
+
+const Mike: User = {
+  login: 'Michael',
+  name : 'Mike',
+  password: 'wutitiputi123',
+  mail: 'wutitiputi123@gmail.com',
+  photo: '../assets/Photos/5.png'
+};
+const John: User = {
+  login: 'JonhyBoy',
+  name : 'John',
+  password: 'bestPass',
+  mail: 'johnyboyofficial@gmail.com',
+  photo: '../assets/Photos/6.png'
+};
+const Dasha: User = {
+  login: 'Dashulka',
+  name : 'Daria',
+  password: 'dashkawow',
+  mail: 'dasha1995@gmail.com',
+  photo: '../assets/Photos/7.png'
 };
