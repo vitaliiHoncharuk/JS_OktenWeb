8e2e5c2d42c50997bbcb5b1fac6e81d1d36921d0
vitalika1234 <vitaliihoncharuk@gmail.com>
2019-05-31 12:34:34 +0300

    small fix

--- Angular[folder]/AngularFirst/Ang/src/app/app.component.ts
+++ Angular[folder]/AngularFirst/Ang/src/app/app.component.ts
@@ -9,7 +9,7 @@ import {CookieService} from 'angular2-cookie/core';
 export class AppComponent {
   constructor(protected _cookieService: CookieService) {}
   isLogin = this.setCookie('isLogin', 'true');
-  names: string[] = ['Vitalii', 'Tanya', 'Olena', 'Alex', 'Oleg', 'George', 'Inna', 'Ola', 'Ira'];
+  names: User[] = [Vitalik, Tanya, Vika, Alex];
   changeCookie(): void {
     this.getObj('isLogin') ? this.setCookie('isLogin', 'false') : this.setCookie('isLogin', 'true');
   }
@@ -23,4 +23,34 @@ export class AppComponent {
     return this._cookieService.getObject(key);
   }
 }
+export interface User {
+  login: string;
+  name: string;
+  password: string;
+  mail: string;
+}
 
+const Vitalik: User = {
+  login: 'vitalika123',
+  name : 'Vitalii',
+  password: 'Vitali4ok',
+  mail: 'vitalii@gmail.com'
+};
+const Tanya: User = {
+  login: 'tanka999',
+  name : 'Tanya',
+  password: 'tanysha123',
+  mail: 'tani4ka@gmail.com'
+};
+const Vika: User = {
+  login: 'vikusa123',
+  name : 'Viktoria',
+  password: 'qwerty123',
+  mail: 'viktoriaPushka@gmail.com'
+};
+const Alex: User = {
+  login: 'CoolBoy228',
+  name : 'Alex',
+  password: 'tryToGuessXD',
+  mail: 'alex1949@gmail.com'
+};
